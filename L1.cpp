58764dde19485cd8ea1e9d996c56b7a761fa4786
Smit-Padhara <padharasmit@gmail.com>
2025-07-09 23:30:09 +0530

    Code Modified

--- L1.cpp
+++ L1.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 using namespace std;
-int main(){
-    cout<<"GitHub";
-}
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            if (s[left++] != s[right--]) return false;
+        }
+        return true;
+    }
+};
